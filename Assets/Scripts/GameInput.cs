03daeb0965ad3debeb02964f2284baee97933936
JacobCohen <yaki_2000@hotmail.com>
2023-02-03 21:16:52 +0200

    Added interact event when pressing interact key

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -2,15 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler InteractEvent;
     private PlayerInputActions playerInputActions;
 
     private void Awake()
     {
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+        
+        playerInputActions.Player.Interact.performed += InteractPerformed;
+    }
+
+    private void InteractPerformed(InputAction.CallbackContext callbackContext)
+    {
+        InteractEvent?.Invoke(this, EventArgs.Empty);
     }
 
     public Vector2 GetNormalizedMovementVector()
