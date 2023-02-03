65576a8ce300f1797b988852292ea2896bcb8516
JacobCohen <yaki_2000@hotmail.com>
2023-02-03 21:03:12 +0200

    Added the ability to interact with a counter

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,51 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private float rotateSpeed = 10f;
     [SerializeField] private GameInput gameInput;
+    [FormerlySerializedAs("layerMask")] [SerializeField] private LayerMask counterLayerMask;
     private bool isWalking;
+    private Vector3 lastInteractDir;
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleInteractions();
+    }
     
-    private void Update() {
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
+    private void HandleInteractions()
+    {
+        var inputVector = gameInput.GetNormalizedMovementVector();
+
+        var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        const float interactDistance = 2f;
+
+        if (moveDir != Vector3.zero)
+        {
+            lastInteractDir = moveDir;
+        }
+        
+        if (Physics.Raycast(transform.position, lastInteractDir, out var raycastHit, interactDistance, counterLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                clearCounter.Interact();
+            }
+        }
+    }
+
+    private void HandleMovement()
+    {
         var inputVector = gameInput.GetNormalizedMovementVector();
 
         var moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
@@ -19,35 +55,39 @@ public class Player : MonoBehaviour
         var playerRadius = .7f;
         var playerHeight = 2f;
         var transformPosition = transform.position;
-        var canMove = !Physics.CapsuleCast(transformPosition, transformPosition + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+        var canMove = !Physics.CapsuleCast(transformPosition, transformPosition + Vector3.up * playerHeight, playerRadius,
+            moveDir, moveDistance);
 
-        if (!canMove) {
+        if (!canMove)
+        {
             var moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transformPosition, transformPosition + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+            canMove = !Physics.CapsuleCast(transformPosition, transformPosition + Vector3.up * playerHeight, playerRadius,
+                moveDirX, moveDistance);
 
-            if (canMove) {
+            if (canMove)
+            {
                 moveDir = moveDirX;
-            } else {
+            }
+            else
+            {
                 var moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transformPosition, transformPosition + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+                canMove = !Physics.CapsuleCast(transformPosition, transformPosition + Vector3.up * playerHeight,
+                    playerRadius, moveDirZ, moveDistance);
 
-                if (canMove) {
+                if (canMove)
+                {
                     moveDir = moveDirZ;
                 }
             }
         }
 
-        if (canMove) {
+        if (canMove)
+        {
             transform.position += moveDir * moveDistance;
         }
 
         isWalking = moveDir != Vector3.zero;
 
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
-    }
-
-    public bool IsWalking()
-    {
-        return isWalking;
     }
 }
