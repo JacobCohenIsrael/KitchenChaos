01cecf1a56d0453676848520f70f351805b0f362
JacobCohen <yaki_2000@hotmail.com>
2023-02-04 08:51:38 +0200

    Added the ability to spawn kitchen objects from counters

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private SelectedCounterVisual selectedCounterVisual;
+
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+    [SerializeField] private Transform counterTop;
     
     public void Interact()
     {
-        Debug.Log("Interact");
+        var kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTop);
+        kitchenObjectTransform.localPosition = Vector3.zero;
     }
 
     public void Select()
