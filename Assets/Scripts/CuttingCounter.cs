696f70b2a16e5f0c40d365c5a36c0b248ba7a122
JacobCohen <yaki_2000@hotmail.com>
2023-02-05 18:33:31 +0200

    Added cutting recipe logic for cutting counter

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class CuttingCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO cutKitchenObjectSO;
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipieSOList;
     public override void Interact(Player player)
     {
         if (HasKitchenObject())
@@ -18,18 +20,32 @@ public class CuttingCounter : BaseCounter
         {
             if (player.HasKitchenObject())
             {
-                player.GetKitchenObject().SetKitchenObjectParent(this);
+                if (HasRecipeResult(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
             }
         }
     }
 
     public override void AltInteract(Player player)
     {
-        if (HasKitchenObject())
+        if (HasKitchenObject() && HasRecipeResult(GetKitchenObject().GetKitchenObjectSO()))
         {
+            var outputKitchenObjectSO = GetRecipeResult(GetKitchenObject().GetKitchenObjectSO());
             GetKitchenObject().DestroySelf();
 
-            KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
         }
+    }
+
+    private bool HasRecipeResult(KitchenObjectSO inputKitchenObjectSO)
+    {
+        return (from cuttingRecipeSO in cuttingRecipieSOList where cuttingRecipeSO.input == inputKitchenObjectSO select cuttingRecipeSO.output).FirstOrDefault();
+    }
+
+    private KitchenObjectSO GetRecipeResult(KitchenObjectSO inputKitchenObjectSO)
+    {
+        return (from cuttingRecipeSO in cuttingRecipieSOList where cuttingRecipeSO.input == inputKitchenObjectSO select cuttingRecipeSO.output).FirstOrDefault();
     }
 }
---
+++ Assets/Scripts/CuttingRecipeSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Recipe/Cutting")]
+public class CuttingRecipeSO : ScriptableObject
+{
+    public KitchenObjectSO input;
+    public KitchenObjectSO output;
+}
